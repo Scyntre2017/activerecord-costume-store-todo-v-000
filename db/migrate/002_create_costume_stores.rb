90262781310a43ee299ce681195774be2b7ec8a6
Christopher Jones <cjones999983@gmail.com>
2018-06-17 23:02:17 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,11 @@
-# Create your costume_stores migration here
+class CreateCostumesStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.integer :size
+      t.text :image_url
+      t.timestamps
+    end
+  end
+end
